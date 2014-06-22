9f5c5f64df31aab576dbcb2e4e323b0b724ef12f
Mirko Akov <mupkoo@gmail.com>
2014-06-22 22:04:56 +0300

    Solution to the eleventh task - HTTP File Server

---
+++ 11_http_file_server.js
@@ -0,0 +1,11 @@
+var http = require('http'),
+    fs = require('fs'),
+    port = process.argv[2],
+    fileName = process.argv[3],
+    server;
+
+server = http.createServer(function (request, response) {
+    fs.createReadStream(fileName).pipe(response);
+});
+
+server.listen(port);
